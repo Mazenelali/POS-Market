ef0656117624db714732b086ae4529f6440642b0
Mazen El Ali <mazenelali>
2024-04-26 19:55:04 +0300

    continue install packeges

--- pos-frontend/src/App.tsx
+++ pos-frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      ho
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route>
+        
+      </Route>
+  </Routes>
   );
 }
 
